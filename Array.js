50e31955ae5c14c63ecff1954ee609e0a4206bf1
Rokibul-Hossan-Md-Sohaib <rokibulhossan32@gmail.com>
2024-09-10 20:57:37 +0600

    branch new code

--- Array.js
+++ Array.js
@@ -2,7 +2,3 @@ var table = ["laptop", "pen","watch", 200]
 console.log(table.length);
 table.push("books")
 console.log(table.length)
-table.pop()
-console.log(table)
-table.pop()
-console.log(table)
